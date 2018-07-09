e8e4aefaf04aba0b6eaed1b8599d9272c100ed9f
raunogroch <raul.noguera.rocha@gmail.com>
2018-07-09 17:46:09 -0400

    adding dashboard and all components to full functionality

---
+++ resources/views/layout/navigation.blade.php
@@ -0,0 +1,10 @@
+<ul class="list-unstyled full-box dashboard-sideBar-Menu">
+    <li>
+        <a href="{{ url('/') }}">
+            <i class="zmdi zmdi-view-dashboard zmdi-hc-fw"></i> Dashboard
+        </a>
+    </li>
+    <li>
+        <a href="representative.html"><i class="zmdi zmdi-google-maps zmdi-hc-fw"></i> Add Dots</a>
+    </li>
+</ul>
