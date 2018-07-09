84e6c1d503fe4467af0082bc65dcac47bf18109a
raunogroch <raul.noguera.rocha@gmail.com>
2018-07-09 16:09:22 -0400

    adding model and migration with .idea restriction

---
+++ database/migrations/2018_07_09_195405_create_dots_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dots', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('latitude');
+            $table->integer('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('dots');
+    }
+}
