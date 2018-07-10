46160970bd012ce99a322d960326e1f5b011757d
raunogroch <raul.noguera.rocha@gmail.com>
2018-07-09 21:15:56 -0400

    adding dots with message to dot

--- resources/views/newdots.blade.php
+++ resources/views/newdots.blade.php
@@ -1,4 +1,73 @@
 @extends('layout.app')
 @section('content')
 
+    <div id="map" class="map"></div>
+
+@endsection
+@section('scripts')
+    <script src="{{ url('assets/js/maps.js') }}"></script>
+    <script>
+        function allMap() {
+            var map = new google.maps.Map(
+                document.getElementById('map'),
+                {
+                    zoom: 2,
+                    center: {
+                        lat:0,
+                        lng:0
+                    }
+                });
+
+            map.addListener('click', function(e) {
+                addMarker(e.latLng.lat(), e.latLng.lng());
+            });
+
+            var contentString = '<div class="content_info">\n' +
+                '        <div class="col-sm-12 text-center">\n' +
+                '            <div class="row">\n' +
+                '                <div class="col-sm-12 my-5 line_bottom">\n' +
+                '                    <h5>have you been clicked here!\n</h5>' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '            <div class="row">\n' +
+                '                <div class="col-sm-12 my-10 padding_none">\n' +
+                '                    <span class="pull-left">Name :</span>\n' +
+                '                    <input class="border_input pull-right" id="info_dot" data-lat="" data-lng="" type="text" name="name">\n' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '            <div class="row">\n' +
+                '                <div class="col-sm-12">\n' +
+                '                    <a class="btn btn-success btn-xs" href="#">Load</a>\n' +
+                '                    <a class="btn btn-danger btn-xs" href="#">Delete</a>\n' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '        </div>\n' +
+                '    </div>';
+
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+
+            function addMarker(lat, lng) {
+                var marker = new google.maps.Marker({
+                    position: {
+                        lat: lat,
+                        lng: lng
+                    },
+                    map: map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        scale: 3,
+                        strokeColor: '#ff0000',
+                        strokeWeight: 3,
+                        // fillColor: '#ff0000',
+                        fillOpacity:1
+                    }
+                });
+                infowindow.open(map, marker);
+            }
+        }
+    </script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCz_6QoQyLUUV8S6i2DkNbGWwa9FAsa_PU&callback=allMap">
+    </script>
 @endsection
